8df8de51d5112358be4d974bf9da10b9a1313426
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-12-19 20:46:35 -0800

    add shotchart in main component

---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ShotChart } from "./ShotChart";
+import nba from 'nba';
+
+export class Main extends React.Component {
+    state = {
+        playerId: nba.findPlayer("Stephen Curry").playerId
+    }
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <ShotChart playerId={this.state.playerId}/>
+            </div>
+        );
+    }
+}
