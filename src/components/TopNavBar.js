eef583455e2c6d7e37e95a071e9880ea5ee3accc
Jialun Lin <jialunlin@Jialuns-MBP.hsd1.ca.comcast.net>
2017-12-19 20:10:44 -0800

    add topnavbar style

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from '../assets/images/logo.svg';
+import logo from '../assets/images/nba-logoman-word-white.svg';
+
 
 export class TopNavBar extends React.Component {
     render() {
         return (
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
             </header>
         );
     }
