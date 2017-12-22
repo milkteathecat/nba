97e077588dec5d060e391d667b9676fcd1b2a52b
Jialun Lin <jialunlin@Jialuns-MacBook-Pro.local>
2017-12-21 20:28:04 -0800

    add minCount to connect shot chart and slider

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -3,12 +3,22 @@ import { ShotChart } from "./ShotChart";
 import { CountSlider } from "./CountSlider";
 
 export class DataViewContainer extends React.Component {
+    state = {
+        minCount: 2
+    }
+
+    onCountSliderChange = (value) => {
+        this.setState({ minCount: value });
+    }
+
     render() {
         return(
            <div className="data-view">
-                <ShotChart playerId={this.props.playerId}/>
+                <ShotChart playerId={this.props.playerId} minCount={this.state.minCount}/>
                <div className="filters">
-                    <CountSlider/>
+                    <CountSlider
+                        minCount={this.state.minCount}
+                        onCountSliderChange={this.onCountSliderChange}/>
                </div>
            </div>
         );
