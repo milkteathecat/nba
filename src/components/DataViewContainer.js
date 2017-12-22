ad1b590eeff5786d77fdc26b6a500c550f3bcba7
Jialun Lin <jialunlin@Jialuns-MacBook-Pro.local>
2017-12-21 19:55:15 -0800

    add sliders as filter

---
+++ src/components/DataViewContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ShotChart } from "./ShotChart";
+import { CountSlider } from "./CountSlider";
+
+export class DataViewContainer extends React.Component {
+    render() {
+        return(
+           <div className="data-view">
+                <ShotChart playerId={this.props.playerId}/>
+               <div className="filters">
+                    <CountSlider/>
+               </div>
+           </div>
+        );
+    }
+}
